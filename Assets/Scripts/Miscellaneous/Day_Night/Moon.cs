59cf338252e90e872c827710085fde5b00ad0285
Vinai Suresh <vinaisuresh9@gmail.com>
2013-01-24 12:56:33 -0500

    Suns and Moon brightness
    Sun and Moon change brightness while they are setting and rising. Soon
    I will make it so the sunflare color changes while it is setting and
    rising

---
+++ Assets/Scripts/Miscellaneous/Day_Night/Moon.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+
+[AddComponentMenu("Environments/Moon")]
+public class Moon : MonoBehaviour {
+	public float maxLightBrightness;
+	public float minLightBrightness;
+	
+	public float maxFlareBrightness;
+	public float minFlareBrightness;
+	
+	
+	void Awake()
+    {
+        SunManager.Main.AddLight(this);  // Register with the night light manager.
+    }
+	
+	
+}
--- Assets/Scripts/Miscellaneous/Day_Night/SunManager.cs
+++ Assets/Scripts/Miscellaneous/Day_Night/SunManager.cs
@@ -1,34 +1,88 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [DefaultSceneObject("SunManager")]
+
+/// <summary>
+/// Sun manager.
+/// This class is used to create variable brightness for the sun and moon. As each of them are rising they will increase in brightness, and as each of
+/// them are setting they will decrease in brightness. Also the sun flare will change color to simulate a real sunset.
+/// </summary>
 public class SunManager : MonoBehaviour {
 	
-	public LinkedList<NightLight> lights = new LinkedList<NightLight>();
+	
+	public LinkedList<Sun> suns = new LinkedList<Sun>();
+	public LinkedList<Moon> moons = new LinkedList<Moon>();
 
-    private static NightLightManager _instance;
-    public static NightLightManager Main { 
+    private static SunManager _instance;
+    public static SunManager Main { 
         get {
         if (_instance == null)
         {
-            _instance = new GameObject("NightLightManager").AddComponent<NightLightManager>();
+            _instance = new GameObject("SunManager").AddComponent<SunManager>();
         }
         return _instance;
     }
     }
-
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
+	//Update is called once per frame
 	void Update () {
+		
+		float currentTime = GameTime.CurrentTime;
+
+        if (currentTime > 17 && currentTime < 18)
+        {
+            float currentPercentage = currentTime - 17f;
+            foreach (Sun light in suns)
+            {
+                light.light.intensity = Mathf.Lerp(light.maxLightBrightness, light.minLightBrightness, currentPercentage);
+				light.GetComponent<LensFlare>().brightness = Mathf.Lerp(light.maxFlareBrightness, light.minFlareBrightness, currentPercentage);
+            }
+			
+        }
+		else if (currentTime > 18.2 && currentTime < 19.2)
+		{
+			float currentPercentage = currentTime - 18.2f;
+			foreach (Moon light in moons)
+            {
+                light.light.intensity = Mathf.Lerp(light.minLightBrightness, light.maxLightBrightness, currentPercentage);
+				light.GetComponent<LensFlare>().brightness = Mathf.Lerp(light.minFlareBrightness, light.maxFlareBrightness, currentPercentage);
+            }
+			
+		}
+		
+		else if (currentTime > 4.7 && currentTime < 5.7)
+		{
+			float currentPercentage = currentTime - 4.7f;
+			foreach (Moon light in moons)
+            {
+                light.light.intensity = Mathf.Lerp(light.maxLightBrightness, light.minLightBrightness, currentPercentage);
+				light.GetComponent<LensFlare>().brightness = Mathf.Lerp(light.maxFlareBrightness, light.minFlareBrightness, currentPercentage);
+            }
+			
+		}
+		else if (currentTime > 6.3 && currentTime < 7.3)
+		{
+			float currentPercentage = currentTime - 6.3f;
+			foreach (Sun light in suns)
+            {
+                light.light.intensity = Mathf.Lerp(light.minLightBrightness, light.maxLightBrightness, currentPercentage);
+				light.GetComponent<LensFlare>().brightness = Mathf.Lerp(light.minFlareBrightness, light.maxFlareBrightness, currentPercentage);
+            }
+			
+		}
+		
 	
 	}
 	
-	public void AddLight(NightLight l)
+	public void AddLight(Sun s)
     {
-        lights.AddLast(l);
+        suns.AddLast(s);
     }
+	
+	public void AddLight(Moon m)
+	{
+		moons.AddLast(m);
+	}
 }

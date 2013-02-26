d38524ec83ec3a82aa5a3bd92ef1daf25bc9a4c4
Eric Mellino <raverenx@gmail.com>
2013-02-26 04:21:01 -0500

    put fading floating damage/ heal numbers back in

--- Assets/Scripts/PlayerScripts/FloatingDamageNumber.cs
+++ Assets/Scripts/PlayerScripts/FloatingDamageNumber.cs
@@ -4,6 +4,9 @@ public class FloatingDamageNumber : SimpleObjectLabel
 {
     public int amount;
     float heightPerSecond = 1f;
+           float elapsedTime = 0f;
+    float maxTime = 3.0f;
+    float fadePoint;
 
     protected override void Start()
     {
@@ -23,11 +26,27 @@ public class FloatingDamageNumber : SimpleObjectLabel
         offset += Vector3.forward * Random.Range(-.1f, .1f);
         offset += Vector3.right * Random.Range(-.1f, .1f);
 
-
+        fadePoint = maxTime * .75f;
     }
 
     void Update()
     {
         offset += Vector3.up * (heightPerSecond * Time.deltaTime);
+    }
+
+    protected override void FadeTextColor()
+    {
+        base.FadeTextColor();
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= fadePoint)
+        {
+            var percent = (elapsedTime - fadePoint) / (maxTime - fadePoint);
+            var color = guiText.material.color;
+            var prevAlpha = color.a;
+            var alpha = prevAlpha - (prevAlpha) * percent;
+            color.a = alpha;
+            guiText.material.color = color;
+
+        }
     }
 }
--- Assets/Scripts/PlayerScripts/FloatingHealNumber.cs
+++ Assets/Scripts/PlayerScripts/FloatingHealNumber.cs
@@ -4,13 +4,17 @@ public class FloatingHealNumber : SimpleObjectLabel
 {
     public int amount;
     float heightPerSecond = 1f;
+       float elapsedTime = 0f;
+    float maxTime = 3.0f;
+    float fadePoint;
+
 
     protected override void Start()
     {
         base.Start();
         guiText.fontSize = 36;
         guiText.material.color = Color.green;
-        Destroy(this.gameObject, 3.0f);
+        Destroy(this.gameObject, maxTime);
 
         float percentage = ((float)amount / (float)transform.root.gameObject.GetCharacterStats().Health.MaxValue);
         guiText.text = amount.ToString();
@@ -24,11 +28,28 @@ public class FloatingHealNumber : SimpleObjectLabel
         offset += Vector3.forward * Random.Range(-.1f, .1f);
         offset += Vector3.right * Random.Range(-.1f, .1f);
 
-
+        fadePoint = maxTime * .75f;
     }
 
     void Update()
     {
         offset += Vector3.up * (heightPerSecond * Time.deltaTime);
     }
+
+    protected override void FadeTextColor()
+    {
+        base.FadeTextColor();
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= fadePoint)
+        {
+            var percent = (elapsedTime - fadePoint) / (maxTime - fadePoint);
+            var color = guiText.material.color;
+            var prevAlpha = color.a;
+            var alpha = prevAlpha - (prevAlpha) * percent;
+            color.a = alpha;
+            guiText.material.color = color;
+
+        }
+    }
+
 }
